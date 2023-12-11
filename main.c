89095d800950e78bcb0aa1991c2928657479eb2f
davidddeveloper <programmingdonebydavid@gmail.com>
2023-12-11 05:55:58 -0800

    handle_env.c: handle environments

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
  * main - simple shell
  * @ac: arguments coun
  * @av: an array of strings of argument
+ * @envp: array of strings of environment variables
  *
  * Return: 0 always
  */
@@ -13,9 +14,6 @@ int main(int ac, char **av, char *envp[])
 	char *user_input;
 	size_t size_of_command = 32;
 	const char *str = "$ ";
-	char **copy_env;
-	unsigned int i;
-	size_t length;
 
 	(void)ac;
 	(void)av;
@@ -36,20 +34,8 @@ int main(int ac, char **av, char *envp[])
 
 			/* handling the builtin env */
 			if (strcmp(user_input, "env\n") == 0)
-			{
-				/* allocating memory for copy_env */
-				copy_env = malloc(sizeof(char *) * MAX_TOKENS);
+				handle_env(envp);
 
-				for (i = 0; envp[i] != NULL; i++)
-				{
-					copy_env[i] = strdup(envp[i]);
-					length = sizeof(envp[i]) - 1;
-					strcat(copy_env[i], "\n");
-					write(1, copy_env[i], length);
-				}
-
-				free(copy_env);
-			}
 			/* execute the command */
 			execmd(user_input);
 		}
