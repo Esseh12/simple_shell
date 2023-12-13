968e2239bf9b2087f18d26d4eb63dc2c2741a1c5
Esseh12 <gbenmoese788@gmai.com>
2023-12-13 13:46:38 -0800

    testing testing

--- execmd.c
+++ execmd.c
@@ -1,18 +1,65 @@
-#include "main.h"
+#include "shell.h"
 
-void execmd(char **argv){
-    char *command = NULL, *actual_command = NULL;
+/**
+ * execmd - for executing the command
+ * @user_input: the command to execute
+ *
+ * Return: 0 always
+ */
+void execmd(char *user_input)
+{
+	char *command;
+	char **commands;
+	char *delim = " ";
+	int counter = 0;
+	int token_num = 0;
+	int pid;
+	char *actual_command;
 
-    if (argv){
-        /* get the command */
-        command = argv[0];
+	/* Remove the \n in the command */
+	user_input = strtok(user_input, "\n");
 
-        /* generate the path to this command before passing it to execve */
-        actual_command = get_location(command);
+	/* tokenize command using strtok */
+	command = strtok(user_input, delim);
 
-/* execute the actual command with execve */
-if (execve(actual_command, argv, NULL) == -1){
-perror("Error:");
-}
-}
+	/* checking number of words in commands */
+	while (command != NULL)
+	{
+		token_num++;
+		command = strtok(NULL, delim);
+	
+	}
+
+	token_num++;
+	/* allocate memory for commands */
+	commands = malloc(sizeof(char *) * token_num);
+	command = strtok(user_input, delim);
+
+	/* keep tokenizing the command */
+	while (command != NULL)
+	{
+		commands[counter] = strdup(command);
+		command = strtok(NULL, delim);
+		counter++;
+	}
+
+	/* create a new process that will execute the command */
+	actual_command = get_local(commands[0]);
+	if (actual_command != NULL)
+	{
+		pid = fork();
+		wait(NULL);
+
+		if (pid == 0)
+			if (execve(actual_command, commands, NULL) == -1)
+				perror("./shell");
+	}
+	else
+		perror("./shell");
+
+	while (counter--)
+	{
+		commands[counter] = NULL;
+		free(commands[counter]);
+	}
 }
