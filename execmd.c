d03fee9c31830a8ab3c20689705eadb5790f7edf
davidddeveloper <programmingdonebydavid@gmail.com>
2023-12-14 02:52:38 -0800

    created a child process for the execve function

--- execmd.c
+++ execmd.c
@@ -10,19 +10,24 @@ void execmd(char *av[])
 {
 	char *command = NULL;
 	char *actual_command = NULL;
+	pid_t pid;
 
 	if (av)
 	{
-		/* get the command */
-		command = av[0];
-
-		/* generate the path to this command before passing it to execve */
-		actual_command = get_local(command);
-
-		/* execute the actual command with execve */
-		if (execve(actual_command, av, NULL) == -1)
+		pid = fork();
+		if (pid == 0)
 		{
-			perror("Error:");
+			/* get the command */
+			command = av[0];
+
+			/* generate the path to this command before passing it to execve */
+			actual_command = get_local(command);
+
+			/* execute the actual command with execve */
+			if (execve(actual_command, av, NULL) == -1)
+				perror("Error:");
 		}
+		else
+			wait(NULL);
 	}
 }
