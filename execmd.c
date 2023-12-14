cb1b7cc4cab48eeeaa6015b04dda94af7008bef5
Esseh12 <gbenmoese788@gmai.com>
2023-12-13 17:25:02 -0800

    proud

--- execmd.c
+++ execmd.c
@@ -2,32 +2,27 @@
 
 /**
  * execmd - for executing the command
- * @user_input: the command to execute
+ * @av: the command to execute
  *
  * Return: 0 always
  */
 void execmd(char *av[])
 {
 	char *command = NULL;
-	int pid; 
 	char *actual_command = NULL;
 
-	if (av){
-        /* get the command */
-        	command = av[0];
+	if (av)
+	{
+		/* get the command */
+		command = av[0];
 
-        /* generate the path to this command before passing it to execve */
-        	actual_command = get_local(command);
+		/* generate the path to this command before passing it to execve */
+		actual_command = get_local(command);
 
-		pid = fork();
-		/* parent process */
-		if (pid != 0)
-			wait(NULL);
-		/* child process */
-		if (pid == 0)
-			/* execute the actual command with execve */
-        		if (execve(actual_command, av, NULL) == -1){
-            			perror("Error:");
-        }
-    }
-} 
+		/* execute the actual command with execve */
+		if (execve(actual_command, av, NULL) == -1)
+		{
+			perror("Error:");
+		}
+	}
+}
--- main.c
+++ main.c
@@ -18,9 +18,7 @@ int main(int ac, char **av, char *envp[])
 	char *command = NULL;
 	const char *delim = " \n";
 	int token_num = 0;
-	/* int counter = 0; */
 	int i;
-
 	(void)ac;
 	(void)envp;
 
@@ -31,48 +29,37 @@ int main(int ac, char **av, char *envp[])
 		num_char = getline(&user_input, &size_of_command, stdin);
 		if (num_char == -1)
 			return (-1);
-		/* exiting the shell */
 		/* allocate a space for user_input_copy */
 		user_input_copy = malloc(sizeof(char) * num_char);
 		strcpy(user_input_copy, user_input);
 		command = strtok(user_input, delim);
-
-        	/* checking number of words in commands */
-        	while (command != NULL)
-        	{
-                	token_num++;
-                	command = strtok(NULL, delim);
-
-        	}
-
-        	token_num++;
-        	/* allocate memory for commands */
-        	av = malloc(sizeof(char *) * token_num);
-        	command = strtok(user_input_copy, delim);
-
-        	/* keep tokenizing the command */
-        	for (i = 0; command != NULL; i++)
-        	{
-			
-                	av[i] = malloc(sizeof(char) * strlen(command));
+		/* checking number of words in commands */
+		while (command != NULL)
+		{
+			token_num++;
+			command = strtok(NULL, delim);
+		}
+		token_num++;
+		/* allocate memory for commands */
+		av = malloc(sizeof(char *) * token_num);
+		command = strtok(user_input_copy, delim);
+		/* keep tokenizing the command */
+		for (i = 0; command != NULL; i++)
+		{
+			av[i] = malloc(sizeof(char) * strlen(command));
 			strcpy(av[i], command);
-                	command = strtok(NULL, delim);
-        	}
+			command = strtok(NULL, delim);
+		}
 		av[i] = NULL;
-
 		if (strcmp(user_input, "exit") == 0)
 			return (0);
-
-		 /* handling the builtin env */
+		/* handling the builtin env */
 		if (strcmp(user_input, "env") == 0)
 			handle_env(envp);
-		
 		/* execute the command */
 		execmd(av);
-	} 
-
+	}
 	free(user_input);
 	free(user_input_copy);
-
-	return (0);
+return (0);
 }
