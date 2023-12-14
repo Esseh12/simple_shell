654fcf78337738a29bc6939f6d978fc4da9eeb58
Esseh12 <gbenmoese788@gmai.com>
2023-12-13 16:29:08 -0800

    test 5

--- execmd.c
+++ execmd.c
@@ -8,60 +8,49 @@
  */
 void execmd(char *av[])
 {
-	char *command;
+	char *command = NULL;
 	/** char **commands; 
 	char *delim = " ";
 	int counter = 0;
-	int token_num = 0; */
-	int pid;
-	char *actual_command;
+	int token_num = 0; 
+	int pid; */
+	char *actual_command = NULL;
 
-	/**Remove the \n in the command 
-	av[0] = strtok(av[0], "\n"); */
-
-	/** tokenize command using strtok 
-	command = strtok(user_input, delim);
-
-	* checking number of words in commands 
-	while (command != NULL)
-	{
-		token_num++;
-		command = strtok(NULL, delim);
-	
-	}
-
-	token_num++;
-	 allocate memory for commands 
-	commands = malloc(sizeof(char *) * token_num);
-	command = strtok(user_input, delim);
-
-	 keep tokenizing the command 
-	while (command != NULL)
-	{
-		commands[counter] = strdup(command);
-		command = strtok(NULL, delim);
-		counter++;
-	}*/
-
-	command = av[0];
-	/* create a new process that will execute the command */
-	actual_command = get_local(command);
-	if (actual_command != NULL)
+	/** command = av[0];
+	create a new process that will execute the command 
+	actual_command = get_local(command); */
+     /**if (actual_command != NULL)
 	{
 		pid = fork();
 		wait(NULL);
 
 		if (pid == 0)
-			if (execve(actual_command, av, NULL) == -1)
+
+		if (execve(actual_command, av, NULL) == -1)
 				perror("./shell");
 	}
 	else
 		perror("./shell");
-	/*av[counter] = NULL;*/
+	av[counter] = NULL;*/
 /**
 	while (counter--)
 	{
 		commands[counter] = NULL;
 		free(commands[counter]);
 	}*/
-}
+
+	if (av){
+        /* get the command */
+        	command = av[0];
+
+        /* generate the path to this command before passing it to execve */
+        	actual_command = get_local(command);
+
+        /* execute the actual command with execve */
+        	if (execve(actual_command, av, NULL) == -1){
+            		perror("Error:");
+        }
+    }
+} 
+
+
