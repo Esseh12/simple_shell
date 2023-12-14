d32d63fefa9be3be0a6fe652c5640e06cfd33cfc
davidddeveloper <programmingdonebydavid@gmail.com>
2023-12-14 03:51:10 -0800

    removed the process created in execmd

--- execmd.c
+++ execmd.c
@@ -14,20 +14,14 @@ void execmd(char *av[])
 
 	if (av)
 	{
-		pid = fork();
-		if (pid == 0)
-		{
-			/* get the command */
-			command = av[0];
+		/* get the command */
+		command = av[0];
 
-			/* generate the path to this command before passing it to execve */
-			actual_command = get_local(command);
+		/* generate the path to this command before passing it to execve */
+		actual_command = get_local(command);
 
-			/* execute the actual command with execve */
-			if (execve(actual_command, av, NULL) == -1)
-				perror("./shell");
-		}
-		else
-			wait(NULL);
+		/* execute the actual command with execve */
+		if (execve(actual_command, av, NULL) == -1)
+			perror("./shell");
 	}
 }
