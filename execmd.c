4bdffc36883e5504039d8dda8e23e167b2c3b753
Esseh12 <gbenmoese788@gmai.com>
2023-12-13 16:45:29 -0800

    test 7

--- execmd.c
+++ execmd.c
@@ -9,35 +9,8 @@
 void execmd(char *av[])
 {
 	char *command = NULL;
-	/** char **commands; 
-	char *delim = " ";
-	int counter = 0;
-	int token_num = 0; 
-	int pid; */
+	int pid; 
 	char *actual_command = NULL;
-
-	/** command = av[0];
-	create a new process that will execute the command 
-	actual_command = get_local(command); */
-     /**if (actual_command != NULL)
-	{
-		pid = fork();
-		wait(NULL);
-
-		if (pid == 0)
-
-		if (execve(actual_command, av, NULL) == -1)
-				perror("./shell");
-	}
-	else
-		perror("./shell");
-	av[counter] = NULL;*/
-/**
-	while (counter--)
-	{
-		commands[counter] = NULL;
-		free(commands[counter]);
-	}*/
 
 	if (av){
         /* get the command */
@@ -46,11 +19,15 @@ void execmd(char *av[])
         /* generate the path to this command before passing it to execve */
         	actual_command = get_local(command);
 
-        /* execute the actual command with execve */
-        	if (execve(actual_command, av, NULL) == -1){
-            		perror("Error:");
+		pid = fork();
+		/* parent process */
+		if (pid != 0)
+			wait(NULL);
+		/* child process */
+		if (pid == 0)
+			/* execute the actual command with execve */
+        		if (execve(actual_command, av, NULL) == -1){
+            			perror("Error:");
         }
     }
 } 
-
-
